1ccf066cc00f1ebd1fddb4098c479725c5395f8e
mogenerale <mogenerale@vauldex.com>
2025-04-23 15:38:57 +0800

    Update style

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>MKGNRL - Portfolio</h1>
+  <div class="w-screen h-[80vh] flex items-center justify-center">
+    <h1 class="font-bold text-6xl">MKGNRL</h1>
   </div>
 </template>
 
